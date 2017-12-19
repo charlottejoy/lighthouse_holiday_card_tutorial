a91737296f233d4d3dd98fecb1fa3646b0418b78
Charlotte Joy Callender <charlottejoyliving@gmail.com>
2017-12-18 20:19:55 -0500

    Part 3 of the tutorial. Learned that unless the <script> reference is at the bottom of the body, I get an error when referring to things in the HTML

--- js/main.js
+++ js/main.js
@@ -5,3 +5,37 @@ function downloadURI(uri, name) {
   link.click();
 }
 
+var cardContainer = document.getElementById('card-container');
+var cardImage = document.getElementById('card-image');
+var cardTitle = document.getElementById('card-title');
+var cardMsg = document.getElementById('card-msg');
+var cardFrom = document.getElementById('card-from');
+
+var formTitle = document.getElementById('title');
+var formFrom = document.getElementById('from');
+var formMsg = document.getElementById('msg');
+var formImage = document.getElementById('image');
+
+var saveButton = document.getElementById('save-button');
+
+formTitle.addEventListener("keyup", function(event){
+cardTitle.innerText = event.target.value;
+console.log(event.target.value);
+});
+
+formFrom.addEventListener('keyup', function(event) {
+  cardFrom.innerText = event.target.value;
+});
+
+formMsg.addEventListener('keyup', function(event) {
+  cardMsg.innerText = event.target.value;
+});
+
+formImage.addEventListener("change", function(event){
+	cardImage.src = event.target.value;
+});
+
+saveButton.addEventListener("click", function(event){
+	event.preventDefault();
+});
+
